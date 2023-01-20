3feebe519e02e75accd6d647a2ffcc6358c82893
Coamox <coamox443@gmail.com>
2023-01-19 21:54:43 -0500

    Extended Car Class and Constructor implemented

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,14 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class Car extends VehicleModule 
+{
+    constructor(make, model, year, color, mileage)
+    {
+        super(make, model, year, color, mileage);
+    }
+}
+
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
