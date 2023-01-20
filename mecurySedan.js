c94e9f8d908189c3ad57fb360d0434e8f8da809e
Coamox <coamox443@gmail.com>
2023-01-19 21:59:32 -0500

    Methods Started

--- mecurySedan.js
+++ mecurySedan.js
@@ -6,7 +6,30 @@ class Car extends VehicleModule
     constructor(make, model, year, color, mileage)
     {
         super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
     }
+
+    loadPassenger(num)
+    {
+
+    }
+
+    start()
+    {
+
+    }
+
+    checkService(mileage)
+    {
+       
+    }
+
+
 }
 
 //this shows how to call from this module...
